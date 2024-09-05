7fec2b129cf0332dc3ad3c705516109aeea8f5bd
sium <siumhaque1089@gmail.com>
2024-09-05 06:59:43 +0600

    4th commit

--- for-of.js
+++ for-of.js
@@ -1,3 +1,10 @@
 const numbers = [12, 98, 45, 63, 21, 72];
 
-console.log(numbers);
+//console.log(numbers);
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruit', 'watermelon'];
+
+//for() {
+
+//}
+
